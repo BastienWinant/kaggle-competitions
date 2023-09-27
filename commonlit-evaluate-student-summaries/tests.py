9c1d75ac8b1e186602d3157db0991e7e5bda48ed
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-09-28 01:33:41 +0200

    switch assertEquals to assertEqual calls (resolve warnings)

--- commonlit-evaluate-student-summaries/tests.py
+++ commonlit-evaluate-student-summaries/tests.py
@@ -21,10 +21,10 @@ class DatasetImportTestCase(unittest.TestCase):
 
     # test datafiles existence
     def test_filepath_existence(self):
-        self.assertEquals(os.path.exists(self.dataset.train_prompts_file), True)
-        self.assertEquals(os.path.exists(self.dataset.test_prompts_file), True)
-        self.assertEquals(os.path.exists(self.dataset.train_summaries_file), True)
-        self.assertEquals(os.path.exists(self.dataset.test_summaries_file), True)
+        self.assertEqual(os.path.exists(self.dataset.train_prompts_file), True)
+        self.assertEqual(os.path.exists(self.dataset.test_prompts_file), True)
+        self.assertEqual(os.path.exists(self.dataset.train_summaries_file), True)
+        self.assertEqual(os.path.exists(self.dataset.test_summaries_file), True)
 
     # test train data read into dataframes
     def test_train_data_read(self):
@@ -51,17 +51,29 @@ class DatasetCleanTestCase(unittest.TestCase):
         prompts_df_columns = prompts_df.columns
         summaries_df_columns = summaries_df.columns
         merge_candidates = prompts_df_columns.intersection(summaries_df_columns)
-        
+
+        # verify that at least one column is shared between the dataframes
         self.assertGreater(len(merge_candidates), 0)
 
+        # verify that no null values are in the merging candidates
+        for column in merge_candidates:
+            self.assertEqual(prompts_df[column].isna().sum(), 0)
+            self.assertEqual(summaries_df[column].isna().sum(), 0)
+
     def test_test_merge_columns(self):
-        prompts_df, summaries_df = self.dataset.load_data(training=True)
+        prompts_df, summaries_df = self.dataset.load_data(training=False)
 
         prompts_df_columns = prompts_df.columns
         summaries_df_columns = summaries_df.columns
         merge_candidates = prompts_df_columns.intersection(summaries_df_columns)
-        
+
+        # verify that at least one column is shared between the dataframes
         self.assertGreater(len(merge_candidates), 0)
+        
+        # verify that no null values are in the merging candidates
+        for column in merge_candidates:
+            self.assertEqual(prompts_df[column].isna().sum(), 0)
+            self.assertEqual(summaries_df[column].isna().sum(), 0)
         
         
 def DatasetImportTestSuite():
@@ -71,6 +83,7 @@ def DatasetImportTestSuite():
     suite.addTest(DatasetImportTestCase('test_filepath_existence'))
     suite.addTest(DatasetImportTestCase('test_train_data_read'))
     return suite
+
 
 def DatasetCleanTestSuite():
     suite = unittest.TestSuite()
