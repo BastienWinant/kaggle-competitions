391a5a17b214301fc05a034463092c1854d65af8
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-09-27 18:58:37 +0200

    add comments to DatasetImportTestCase unit tests

--- commonlit-evaluate-student-summaries/commonlitDataset.py
+++ commonlit-evaluate-student-summaries/commonlitDataset.py
@@ -9,6 +9,11 @@ class Dataset:
         self.train_summaries_file = "./data/summaries_train.csv"
         self.test_summaries_file = "./data/summaries_test.csv"
 
-    def load_training_data(self):
-        self.train_prompts = None
+    def load_data(self, training=True):
+        if training:
+            self.train_prompts = pd.read_csv(self.train_prompts_file)
+            self.train_summaries = pd.read_csv(self.train_summaries_file)
+        else:
+            self.test_prompts = pd.read_csv(self.test_prompts_file)
+            self.test_summaries = pd.read_csv(self.test_summaries_file)
         # self.train_prompts = pd.read_csv(train_prompts_file)
--- commonlit-evaluate-student-summaries/tests.py
+++ commonlit-evaluate-student-summaries/tests.py
@@ -3,28 +3,42 @@ import unittest
 import os
 import pandas as pd
 
+# check the existence of data files
+# and reads into pandas dataframes
 class DatasetImportTestCase(unittest.TestCase):
     def setUp(self):
         self.dataset = Dataset()
-        
+
+    # test Dataset class instantiation
     def test_datatype(self):
         self.assertIsInstance(self.dataset, Dataset)
-    
+
+    # test filepath attributes' datatypes
     def test_filepath_attributes(self):
         self.assertIsInstance(self.dataset.train_prompts_file, str)
         self.assertIsInstance(self.dataset.test_prompts_file, str)
         self.assertIsInstance(self.dataset.train_summaries_file, str)
         self.assertIsInstance(self.dataset.test_summaries_file, str)
-    
+
+    # test datafiles existence
     def test_filepath_existence(self):
         self.assertEquals(os.path.exists(self.dataset.train_prompts_file), True)
         self.assertEquals(os.path.exists(self.dataset.test_prompts_file), True)
         self.assertEquals(os.path.exists(self.dataset.train_summaries_file), True)
         self.assertEquals(os.path.exists(self.dataset.test_summaries_file), True)
 
+    # test train data read into dataframes
     def test_train_data_read(self):
-        self.dataset.load_training_data()
+        self.dataset.load_data()
         self.assertIsInstance(self.dataset.train_prompts, pd.DataFrame)
+        self.assertIsInstance(self.dataset.train_summaries, pd.DataFrame)
+
+    # test test data read into dataframes
+    def test_test_data_read(self):
+        self.dataset.load_data(training=False)
+        self.assertIsInstance(self.dataset.test_prompts, pd.DataFrame)
+        self.assertIsInstance(self.dataset.test_summaries, pd.DataFrame)
+
 
 def suite():
     suite = unittest.TestSuite()
