d124aa288c0328714a12b616d46866704078b72e
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-09-27 18:49:54 +0200

    check data files existence and reads into pandas dfs

--- commonlit-evaluate-student-summaries/.ipynb_checkpoints/tests-checkpoint.py
+++ commonlit-evaluate-student-summaries/.ipynb_checkpoints/tests-checkpoint.py
@@ -1,17 +1,37 @@
 from commonlitDataset import Dataset
 import unittest
+import os
+import pandas as pd
 
-class DatasetTestCase(unittest.TestCase):
+class DatasetImportTestCase(unittest.TestCase):
     def setUp(self):
         self.dataset = Dataset()
         
     def test_datatype(self):
-        # self.assertEqual(type(self.dataset), "<class 'commonlitDataset.Dataset'>")
         self.assertIsInstance(self.dataset, Dataset)
+    
+    def test_filepath_attributes(self):
+        self.assertIsInstance(self.dataset.train_prompts_file, str)
+        self.assertIsInstance(self.dataset.test_prompts_file, str)
+        self.assertIsInstance(self.dataset.train_summaries_file, str)
+        self.assertIsInstance(self.dataset.test_summaries_file, str)
+    
+    def test_filepath_existence(self):
+        self.assertEquals(os.path.exists(self.dataset.train_prompts_file), True)
+        self.assertEquals(os.path.exists(self.dataset.test_prompts_file), True)
+        self.assertEquals(os.path.exists(self.dataset.train_summaries_file), True)
+        self.assertEquals(os.path.exists(self.dataset.test_summaries_file), True)
+
+    def test_train_data_read(self):
+        self.dataset.load_training_data()
+        self.assertIsInstance(self.dataset.train_prompts, pd.DataFrame)
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(DatasetTestCase('test_datatype'))
+    suite.addTest(DatasetImportTestCase('test_datatype'))
+    suite.addTest(DatasetImportTestCase('test_filepath_attributes'))
+    suite.addTest(DatasetImportTestCase('test_filepath_existence'))
+    suite.addTest(DatasetImportTestCase('test_train_data_read'))
     return suite
     
 if __name__ == "__main__":
--- commonlit-evaluate-student-summaries/commonlitDataset.py
+++ commonlit-evaluate-student-summaries/commonlitDataset.py
@@ -3,4 +3,12 @@ import pandas as pd
 
 class Dataset:
     def __init__(self):
-        pass
+        self.train_prompts_file = "./data/prompts_train.csv"
+        self.test_prompts_file = "./data/prompts_test.csv"
+
+        self.train_summaries_file = "./data/summaries_train.csv"
+        self.test_summaries_file = "./data/summaries_test.csv"
+
+    def load_training_data(self):
+        self.train_prompts = None
+        # self.train_prompts = pd.read_csv(train_prompts_file)
